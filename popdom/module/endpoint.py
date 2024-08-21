7972f12c455de2d73323aae1d44674f5e543ecce
cs8u7 <tranductuan247@gmail.com>
2024-08-21 21:34:12 +0700

    tool update feature: endpoint, hidden, document

---
+++ popdom/module/endpoint.py
@@ -0,0 +1,125 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+def fetch_wayback_urls(domain,sample):
+   print('[-] Fetching WayBack')
+   url = f"http://web.archive.org/cdx/search/cdx?url=*.{domain}/*&output=json&collapse=urlkey"
+   try:
+      response = requests.get(url,timeout=600)
+      response.raise_for_status()
+      data = response.json()
+
+      result = [item[2] for item in data[1:]]
+
+      with open(sample, 'a') as file:
+         file.write("\n\n")
+         for item in result:
+            file.write(f"{item}\n")
+   except (requests.RequestException, json.JSONDecodeError):
+      pass
+
+def fetch_commoncrawl_urls(domain,sample):
+   print('[-] Fetching Common Crawl')
+   urls = []
+   url = f"http://index.commoncrawl.org/CC-MAIN-2018-22-index?url=*.{domain}/*&output=json"
+
+   try:
+      response = requests.get(url,timeout=600)
+      response.raise_for_status()
+      for line in response.iter_lines():
+         if line:
+            item = json.loads(line)
+            urls.append(item.get('url'))
+      
+      with open(sample, 'a') as file:
+         file.write("\n\n")
+         for item in urls:
+            file.write(f"{item}\n")
+   except (requests.RequestException, json.JSONDecodeError):
+      pass
+
+def fetch_virustotal_urls(domain,sample):
+   print('[-] Fetching Virus total')
+   urls = []
+   api_key = os.getenv('VT_API_KEY')
+   if not api_key:
+      return
+
+   try:
+      load_dotenv()
+      url = 'https://www.virustotal.com/vtapi/v2/domain/report'
+      params = {'apikey':os.getenv('VT_API_KEY'),'domain':domain}
+      response = requests.get(url, params=params)
+      data = response.json()
+
+      if data['verbose_msg'] == 'Domain not found':
+         return
+
+      undetected_urls = []
+      detected_urls = []
+      if data['detected_urls'].__len__() != 0:
+         for url in data['detected_urls']:
+            detected_urls.append(url['url'])
+      elif data['undetected_urls'].__len__() != 0:
+         for url in data['undetected_urls']:
+            undetected_urls.append(url[0])
+
+      combined_urls = undetected_urls + detected_urls
+      urls = list(set(combined_urls))
+
+      with open(sample, 'a') as file:
+         file.write("\n\n")
+         for item in urls:
+            file.write(f"{item}\n")
+   except (requests.RequestException, json.JSONDecodeError):
+      pass
+
+def fetch_urls(domain,sample):
+   fetch_commoncrawl_urls(domain,sample)
+   fetch_virustotal_urls(domain,sample)
+   fetch_wayback_urls(domain,sample)
+
+   with open(sample, 'r') as file:
+      lines = file.readlines()
+   unique_lines = sorted(set(lines))
+   with open(sample, 'w') as file:
+      file.writelines(unique_lines)
+
+def hidden_and_document_endpoint(endpoint_sample,folder_sample):
+   hidden_sample = folder_sample + '/' + folder_sample + '@hidden.txt'
+   print('[-] Reading URLs')
+   with open(endpoint_sample, 'r') as file:
+      url_lines = file.readlines()
+   with open('module/hidden_wordlist.txt', 'r') as file:
+      hidden_lines = file.readlines()
+
+   print('[-] Hidden Endpoint Filtering')
+   hidden_set = []
+   for url_line in url_lines:
+      for hidden_word in hidden_lines:
+         if hidden_word in url_line:
+            hidden_set.append(url_line)
+            break
+   
+   with open(hidden_sample, 'w') as file:
+      pass
+   with open(hidden_sample, 'w') as file:
+      file.writelines(hidden_set)
+
+   document_sample = folder_sample + '/' + folder_sample + '@document.txt'
+   document_set = []
+   doc_temps = ['.txt','.pdf','.doc','xlsx','xls','ods','docx','ppt','pptx']
+   print('[-] Document Filtering')
+   hidden_set = []
+   for url_line in url_lines:
+      for doc in doc_temps:
+         if doc in url_line:
+            document_set.append(url_line)
+            break
+   
+   with open(document_sample, 'w') as file:
+      pass 
+   with open(document_sample, 'w') as file:
+      file.writelines(document_set)
---
+++ popdom/popdom.py
@@ -0,0 +1,47 @@
+import argparse
+import sys
+import re
+from datetime import datetime
+import os
+import shutil
+
+from module.endpoint import fetch_urls
+from module.endpoint import hidden_and_document_endpoint
+from module.domain import subdomain_discover
+
+def main():
+   parser = argparse.ArgumentParser(description="Automate Reconnaissance Tool")
+   parser.add_argument('-u', type=str, help="URL of target web application.")
+   args = parser.parse_args()
+
+   if len(sys.argv) == 1:
+      parser.print_help()
+      sys.exit(1)
+
+   url_pattern = re.compile(r'^((http|https):\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(:\d+)?(\/\S*)?$')
+
+   if args.u:
+      print(' ____     ___    ____    ____     ___    __  __ \n|  _ \   / _ \  |  _ \  |  _ \   / _ \  |  \/  |\n| |_) | | | | | | |_) | | | | | | | | | | |\/| |\n|  __/  | |_| | |  __/  | |_| | | |_| | | |  | |\n|_|      \___/  |_|     |____/   \___/  |_|  |_|')
+      print('-----------------------------------------------------')
+
+      print('[+] Endpoint Discover')
+      date = datetime.now().date()
+      folder_result = str(date) + '#' + args.u
+      if os.path.exists(folder_result) and os.path.isdir(folder_result):
+         shutil.rmtree(folder_result)
+      os.makedirs(folder_result, exist_ok=True)
+      endpoint_result = folder_result + '/' +folder_result + '@endpoint.txt'
+      with open(endpoint_result, 'w') as file:
+         pass 
+      print(f'[-] Create endpoint result sample {args.u} of {date}')
+      if url_pattern.match(args.u):
+         fetch_urls(args.u,endpoint_result)
+
+      print('[+] Subdomain Discover')
+      subdomain_discover(args.u,endpoint_result,folder_result)
+
+      print('[+] Hidden Endpoints & Document Filtering')
+      hidden_and_document_endpoint(endpoint_result,folder_result)
+
+if __name__ == '__main__':
+  main()
